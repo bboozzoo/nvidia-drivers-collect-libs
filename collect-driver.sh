0c4b47bf84a7daa0badb3748e8bc14a016c6f186
Maciek Borzecki <maciek.borzecki@gmail.com>
2024-08-01 14:28:56 +0200

    Collection scripts
    Signed-off-by: Maciek Borzecki <maciek.borzecki@gmail.com>

---
+++ collect-driver.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+
+set -e
+
+sed -i -e 's/^# deb-src /deb-src /' /etc/apt/sources.list >&2
+grep 'deb-src' /etc/apt/sources.list >&2
+
+apt update
+DEBIAN_FRONTEND=noninteractive apt install dpkg-dev -y >&2
+
+driver=$1
+
+apt source "$driver"
+driver_version=$(echo "$driver" | rev | cut  -d- -f1 | rev)
+binpkglist=$(
+    cat nvidia-graphics-drivers-${driver_version}*.dsc | \
+        awk '/Package-List:/ { dump=1; next } /^[a-zA-Z].*:/ { if (dump==1) { dump=0; next} } // { if (dump==1) { print $1 } }' | \
+        grep -v -- -open\
+          )
+
+echo "-- binary package list:" $binpkglist >&2
+
+DEBIAN_FRONTEND=noninteractive apt install $binpkglist -y >&2
+for p in $binpkglist; do
+    dpkg -L $p |grep -E '\.so' || true
+done
---
+++ collect.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+#RELEASES="18.04 20.04 22.04 24.04"
+RELEASES="18.04"
+
+for release in $RELEASES; do
+    if [ -e "$release-done" ]; then
+        echo "---- skipping $release, already done"
+        continue
+    fi
+    lxc launch "ubuntu:$release" nvidiatest --ephemeral -c limits.cpu=4 -c limits.memory=4GiB
+    driver_versions=$(lxc exec nvidiatest -- sh -c "apt-cache search nvidia-driver | grep nvidia-driver | grep -v -- -open | grep -v -- -server | grep -v -i transition | cut -f1 -d' '")
+    lxc delete --force nvidiatest
+
+    echo "-- release $release"
+    echo "-- driver versions:"
+    echo $driver_versions
+
+    for v in $driver_versions; do
+        if [ -e "$release-$v-done" ]; then
+           echo "--- skipping driver $v, already done"
+           continue
+        fi
+
+        lxc launch "ubuntu:$release" nvidiatest --ephemeral -c limits.cpu=4 -c limits.memory=4GiB
+        lxc exec nvidiatest -- cloud-init status --wait
+        echo "--- container ready"
+        lxc file push collect-driver.sh nvidiatest/root/collect-driver.sh
+        lxc exec nvidiatest -- /bin/sh -c "cd /root; ./collect-driver.sh $v" > "$release-$v.libs"
+        lxc delete --force nvidiatest
+        test -s "$release-$v.libs"
+        touch "$release-$v-done"
+    done
+
+    touch "$release-done"
+done
