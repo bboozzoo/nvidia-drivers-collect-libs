b610b4343e2994d7f0e52a9a8304f2dcea7960e0
Maciek Borzecki <maciek.borzecki@gmail.com>
2024-08-01 15:13:07 +0200

    support 24.04 apt sources file
    Signed-off-by: Maciek Borzecki <maciek.borzecki@gmail.com>

--- collect-driver.sh
+++ collect-driver.sh
@@ -2,8 +2,14 @@
 
 set -e
 
-sed -i -e 's/^# deb-src /deb-src /' /etc/apt/sources.list >&2
-grep 'deb-src' /etc/apt/sources.list >&2
+if [ -e /etc/apt/sources.list.d/ubuntu.sources ]; then
+    # new format fro 24.04+
+    sed -i -e 's/Types: deb$/Types: deb deb-src/' /etc/apt/sources.list.d/ubuntu.sources >&2
+    grep 'deb-src' /etc/apt/sources.list.d/ubuntu.sources >&2
+else
+    sed -i -e 's/^# deb-src /deb-src /' /etc/apt/sources.list >&2
+    grep 'deb-src' /etc/apt/sources.list >&2
+fi
 
 apt update >&2
 DEBIAN_FRONTEND=noninteractive apt install dpkg-dev -y >&2
