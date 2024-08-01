2182ceebc6a04a11047f55cbf6036242c4e6f46f
Maciek Borzecki <maciek.borzecki@gmail.com>
2024-08-01 14:40:54 +0200

    split per release collection
    Signed-off-by: Maciek Borzecki <maciek.borzecki@gmail.com>

---
+++ collect-release.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+set -x
+
+release="$1"
+
+cname="nvidiatest-$(echo $release | tr . -)"
+
+if [ -e "$release-done" ]; then
+    echo "---- skipping $release, already done"
+    exit 0
+fi
+
+lxc launch "ubuntu:$release" "$cname" --ephemeral -c limits.cpu=8 -c limits.memory=8GiB
+driver_versions=$(lxc exec "$cname" -- sh -c "apt-cache search nvidia-driver | grep nvidia-driver | grep -v -- -open | grep -v -- -server | grep -v -i transition | cut -f1 -d' '")
+lxc delete --force "$cname"
+
+echo "-- release $release"
+echo "-- driver versions:"
+echo "$driver_versions" > "$release-drivers"
+echo $driver_versions
+
+for v in $driver_versions; do
+    if [ -e "$release-$v-done" ]; then
+        echo "--- skipping driver $v, already done"
+        continue
+    fi
+
+    lxc launch "ubuntu:$release" "$cname" --ephemeral -c limits.cpu=8 -c limits.memory=8GiB
+    lxc exec "$cname" -- cloud-init status --wait
+    echo "--- container ready"
+    lxc file push collect-driver.sh "$cname"/root/collect-driver.sh
+    lxc exec "$cname" -- /bin/sh -c "cd /root; ./collect-driver.sh $v" > "$release-$v.libs"
+    lxc delete --force "$cname"
+    test -s "$release-$v.libs"
+    touch "$release-$v-done"
+done
+
+touch "$release-done"
--- collect.sh
+++ collect.sh
@@ -6,33 +6,5 @@ set -e
 RELEASES="18.04"
 
 for release in $RELEASES; do
-    if [ -e "$release-done" ]; then
-        echo "---- skipping $release, already done"
-        continue
-    fi
-    lxc launch "ubuntu:$release" nvidiatest --ephemeral -c limits.cpu=8 -c limits.memory=8GiB
-    driver_versions=$(lxc exec nvidiatest -- sh -c "apt-cache search nvidia-driver | grep nvidia-driver | grep -v -- -open | grep -v -- -server | grep -v -i transition | cut -f1 -d' '")
-    lxc delete --force nvidiatest
-
-    echo "-- release $release"
-    echo "-- driver versions:"
-    echo $driver_versions
-
-    for v in $driver_versions; do
-        if [ -e "$release-$v-done" ]; then
-           echo "--- skipping driver $v, already done"
-           continue
-        fi
-
-        lxc launch "ubuntu:$release" nvidiatest --ephemeral -c limits.cpu=8 -c limits.memory=8GiB
-        lxc exec nvidiatest -- cloud-init status --wait
-        echo "--- container ready"
-        lxc file push collect-driver.sh nvidiatest/root/collect-driver.sh
-        lxc exec nvidiatest -- /bin/sh -c "cd /root; ./collect-driver.sh $v" > "$release-$v.libs"
-        lxc delete --force nvidiatest
-        test -s "$release-$v.libs"
-        touch "$release-$v-done"
-    done
-
-    touch "$release-done"
+    ./collect-release "$release"
 done
