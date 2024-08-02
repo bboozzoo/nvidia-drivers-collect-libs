7c55d74fa8bf8de0ba3f6baea803464fa08a500a
Maciek Borzecki <maciek.borzecki@gmail.com>
2024-08-02 14:50:33 +0200

    Either install or use apt-file
    Signed-off-by: Maciek Borzecki <maciek.borzecki@gmail.com>

--- collect-driver.sh
+++ collect-driver.sh
@@ -19,14 +19,22 @@ driver=$1
 
 apt source "$driver" >&2
 driver_version=${DRIVER_VERSION-$(echo "$driver" | sed -e 's/-open//' -e 's/-server//' | rev | cut  -d- -f1 | rev)}
+# shellcheck disable=SC2086
 binpkglist=$(
     cat nvidia-graphics-drivers-${driver_version}*.dsc | \
         awk '/Package-List:/ { dump=1; next } /^[a-zA-Z].*:/ { if (dump==1) { dump=0; next} } // { if (dump==1) { print $1 } }' | \
         grep -v -- -open\
           )
 
-echo "-- binary package list:" $binpkglist >&2
+echo "-- binary package list: $binpkglist" >&2
 
-for p in $binpkglist; do
-    apt-file list $p |awk '/\.so/ { print $2 }' | sort -u || true
-done
+if [ "${USE_INSTALL}" = "y" ]; then
+    DEBIAN_FRONTEND=noninteractive apt install $binpkglist -y >&2
+    for p in $binpkglist; do
+        dpkg -L "$p" |grep -E '\.so' | sort -u || true
+    done
+else
+    for p in $binpkglist; do
+        apt-file list "$p" |awk '/\.so/ { print $2 }' | sort -u || true
+    done
+fi
